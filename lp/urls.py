1be332c31b081b050dc2b9b4082b3e7f6972f7be
keek420 <out0724@stu.kanazawa-u.ac.jp>
2022-11-29 18:21:24 +0900

    first commit

---
+++ lp/urls.py
@@ -0,0 +1,10 @@
+
+#app pl of url routring
+from django.urls import path 
+from . import views
+
+
+app_name = "lp"
+urlpatterns = [
+    path("", views.TopView.as_view())
+]
---
+++ lp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views import generic
+
+
+# Create your views here.
+
+
+class TopView(generic.TemplateView):
+    template_name = "template/top.html"
+
+
